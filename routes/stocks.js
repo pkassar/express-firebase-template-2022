fdbde7582b6c5cd7e92185da725e0fdf401b08aa
Paul Kassar <paulkassar@gmail.com>
2022-05-02 17:28:00 -0400

    Display Bitcoin Prices instead of stock information

--- routes/stocks.js
+++ routes/stocks.js
@@ -8,33 +8,34 @@ var router = express.Router();
 
 const API_KEY = process.env.ALPHAVANTAGE_API_KEY || "abc123" // obtain your own api key and set via environment variable
 
-router.get('/form', function(req, res, next) {
+router.get('/form', function (req, res, next) {
   res.render("stocks_form");
 });
 
-router.post('/dashboard', function(req, res, next) {
+router.post('/dashboard', function (req, res, next) {
   console.log("FORM DATA", req.body)
 
   var symbol = req.body.symbol || "OOPS"
   console.log("STOCK SYMBOL", symbol)
 
-  var requestUrl = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${symbol}&apikey=${API_KEY}` // using string interpolation here, but you could alternatively do concatenation with + operators
+  var requestUrl = `https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_DAILY&symbol=${symbol}&market=USD&apikey=${API_KEY}` // using string interpolation here, but you could alternatively do concatenation with + operators
   console.log("REQUEST URL", requestUrl)
 
   fetch(requestUrl)
-    .then(function(response) {
-        return response.json()
+    .then(function (response) {
+      return response.json()
     })
-    .then(function(data){
+    .then(function (data) {
 
-        console.log("STOCK DATA SUCCESS", Object.keys(data))
+      console.log("STOCK DATA SUCCESS", Object.keys(data))
+      // console.log('data: ',JSON.stringify(data))
 
-        var latestClose = Object.values(data["Time Series (Daily)"])[0]["5. adjusted close"]
-
-        req.flash("success", "Stock Data Request Success!")
-        res.render("stocks_dashboard", {symbol: symbol, data: JSON.stringify(data), latestClose: latestClose});
-      })
-    .catch(function(err){
+      var latestClose = Object.values(data["Time Series (Digital Currency Daily)"])[0]["4a. close (USD)"]
+      latestCloseShort = latestClose.slice(0, -5)
+      req.flash("success", "Stock Data Request Success!")
+      res.render("stocks_dashboard", { symbol: symbol, data: JSON.stringify(data), latestClose: latestCloseShort });
+    })
+    .catch(function (err) {
       console.log("STOCK DATA ERROR:", err)
 
       req.flash("danger", "OOPS, Please check your inputs and try again.")
